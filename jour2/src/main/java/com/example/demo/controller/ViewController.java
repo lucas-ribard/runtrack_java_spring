78e6cb061c99f1f198bfb5bd123f8dc86f9daee9
lucas-ribard <lucas.ribard@laplateforme.io>
2023-09-21 15:02:29 +0200

    Job04 Finished

--- jour2/src/main/java/com/example/demo/controller/ViewController.java
+++ jour2/src/main/java/com/example/demo/controller/ViewController.java
@@ -1,23 +1,30 @@
 package com.example.demo.controller;
 
+import org.springframework.ui.Model;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.lang.reflect.Array;
 
 
 @Controller
 public class ViewController {
-
-
-
-    @GetMapping("/test")
+    @GetMapping("/Main")
     public String view(HttpServletRequest request) {
-        String[] list = {"test","bruh","yeet"};
-        request.setAttribute("messages", list);
-
+        String[] list = {"test","bruh","yeet","yote"};
+        request.setAttribute("listes", list);
         return "view";
+    }
+
+    @PostMapping("/Main")
+    public String greetingSubmit(@RequestParam String firstName, @RequestParam String lastName, Model model) {
+        String message = "Bonjour, " + firstName + " " + lastName + " !";
+        model.addAttribute("message", message);
+
+        return "Response";
     }
 }
 
