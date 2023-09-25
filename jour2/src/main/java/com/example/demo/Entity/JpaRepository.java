70b6c467afab16940dea0bacc1343feba1d1577b
lucas-ribard <lucas.ribard@laplateforme.io>
2023-09-25 10:12:22 +0200

    Job03 Finished, Job04 Started

---
+++ jour2/src/main/java/com/example/demo/Entity/JpaRepository.java
@@ -0,0 +1,5 @@
+package com.example.demo.Entity;
+
+public interface JpaRepository extends org.springframework.data.jpa.repository.JpaRepository<Person, Long> {
+
+}
---
+++ jour2/src/main/java/com/example/demo/Entity/PersonRepository.java
@@ -0,0 +1,7 @@
+package com.example.demo.Entity;
+
+import com.example.demo.Entity.Person;
+
+public interface PersonRepository extends JpaRepository{
+    
+}
